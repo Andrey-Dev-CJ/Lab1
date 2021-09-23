226423b0aa6e83abbd5ab4d3b6569c3cd5901098
Andrey-dev-CJ <andrey.godowanets@gmail.com>
2021-09-23 21:55:25 +0300

    new class

--- Ship.cpp
+++ Ship.cpp
@@ -1,11 +1,153 @@
 #include "Ship.h"
 #include <fstream>
+#include <filesystem>
+
+Ship::Ship()
+{
+	this->init();
+}
+
+void Ship::init()
+{
+	ifstream f;
+	ofstream of;
+	string str, nstr;
+	nstr = "";
+	int cntYaht,cntSubmarine,cntSpeedboat,pos;
+
+	//автоматический ввод
+	f.open("DATA/PATH.pth");
+	if (f) {
+		if (f.peek() != EOF) {
+			while (getline(f, str))
+			{
+				//парсер
+				cout << str << "\n";
+				if (str.find("Yaht")!= std::string::npos) {
+					cntYaht = cntShips(str);
+
+				}
+				else if (str.find("Submarine")!= std::string::npos) {
+					cntSubmarine = cntShips(str);
+
+				}
+				else {
+					cntSpeedboat = cntShips(str);
+				}
+
+			}	
+			f.close();
+		}
+		else {//ручной ввод
+			f.close();
+			this->handInput(of, "DATA/PATH.pth");
+		}
+	}
+
+	//ручной ввод
+	else  {
+		this->handInput(of, "DATA/PATH.pth");
+	}
+	
+	//после ввода проводим заполнение 
+
+	//заполнение листа яхтами
+	f.open("DATA/Yaht.ob");
+	int i = 0;
+	if (f.peek() != EOF) {
+		while (getline(f, str)) {
+			MyYaht.append(Yaht(str));
+			if (i == cntYaht-1)
+				break;
+			i++;
+		}
+	}
+	
+	f.close();
+
+	f.open("DATA/Sboat.ob");
+	i = 0;
+	if (f.peek() != EOF) {
+		while (getline(f, str)) {
+			MySpeedboat.append(SpeedBoat(str));
+			if (i ==  cntSpeedboat-1)
+				break;
+			i++;
+		}
+	}
+
+	f.close();
+}
 void Ship::save()
 {
-	std::ofstream fout("ship.txt");
-	fout <<"Submarin:" <<this->cntSubmarine<<std::endl;
-	fout << "Yaht:" << this->yaht << std::endl;
-	fout << "SpeedBoat:" << this->speeBoat << std::endl;
-	fout << "name:" << this->name << std::endl;
-	fout.close();
+
 }
+void Ship::recovery()
+{
+}
+void Ship::handInput(ofstream& of, string filename)
+{
+	string name;
+	int cnt;
+	of.open(filename, ios::out);
+	int i = 0;
+	while (i<3) {
+
+		std::cout << "Enter type " << std::endl;
+		std::cin >> name;
+		std::cout << "count " << std::endl;
+		std::cin >> cnt;
+
+		of << name << "|" << cnt << "|\n";
+
+		//std::cout << "It's all? Entet Y/N: " << std::endl;
+		//std::cin >> name;
+		//if (name == "Y" || name == "y")
+			//break;
+		i++;
+	}
+	of.close();
+}
+void Ship::initNext() {
+	initYahts();
+	initSubmarines();
+	initSpeedboat();
+}
+
+void Ship::initYahts()
+{
+
+}
+
+void Ship::initSubmarines()
+{
+}
+
+void Ship::initSpeedboat()
+{
+}
+
+int Ship::cntShips(string str)
+{	
+	int val;
+	string nstr = "";
+	int pos = str.find("|");
+	int i = pos + 1;
+	while (str[i] != '|') {
+		nstr += str[i];
+		i++;
+	}
+	
+	return stoi(nstr);
+}
+
+//void Ship::save()
+//{
+//	std::ofstream fout("ship.txt");
+//	fout <<"Submarin:" <<this->cntSubmarine<<std::endl;
+//	fout << "Yaht:" << this->yaht << std::endl;
+//	fout << "SpeedBoat:" << this->speeBoat << std::endl;
+//	fout << "name:" << this->name << std::endl;
+//	fout.close();
+//}
+
--- Ship.h
+++ Ship.h
@@ -1,16 +1,39 @@
 #pragma once
 #include <string>
+#include <iostream>
+
+#include"List.h"
+#include "Yaht.h"
+#include"SpeedBoat.h"
+#include"Submarine.h"
+
 using namespace std;
 //базовый класс
 class Ship
-{
+{	
+
+	List<Yaht>MyYaht;
+	List<Submarine> MySubmarine;
+	List<SpeedBoat> MySpeedboat;
+	
+
 public:
-	int cntSubmarine;
+	Ship();
+	void init();
+	void save();
+	void recovery();
+	void handInput(ofstream& of, string filename);
+	void initNext();
+	void initYahts();
+	void initSubmarines();
+	void initSpeedboat();
+	int cntShips(string str);
+	/*int cntSubmarine;
 	int yaht ;
 	int speeBoat;
 	string name;
 	Ship(const Ship& obj) {
-	
+		cout << "copy constructor " << this << endl;
 		this->cntSubmarine = obj.cntSubmarine;
 		this->speeBoat = obj.speeBoat;
 		this->yaht = obj.yaht;
@@ -19,14 +42,14 @@ public:
 	}
 	void save();
 	Ship() {
-	
-		cntSubmarine = 5;
-		yaht = 4;
-		speeBoat = 3;
+		cout << "standart constructor " << this << endl;
+		cntSubmarine = 0;
+		yaht = 0;
+		speeBoat = 0;
 		string name = "no name";
 	}
 	Ship(string val) {
-
+		cout << "constructor with 1 parm " << this << endl;
 		this->name = val;
 		this->cntSubmarine = 5;
 		this->yaht = 4;
@@ -34,5 +57,28 @@ public:
 
 	}
 
+	Ship(string val, int cnt, int cnt2, int cnt3) {
+		cout << "constructor with many parm " << this << endl;
+		this->name = val;
+		this->cntSubmarine = cnt;
+		this->yaht = cnt2;
+		this->speeBoat = cnt3;
+
+	}
+	Ship& operator=(const Ship& obj) {
+		
+		this->name = obj.name;
+		this->cntSubmarine = obj.cntSubmarine;
+		this->yaht = obj.yaht;
+		this->speeBoat = obj.speeBoat;
+		return *this;
+	}
+	~Ship() {
+		this->name = "";
+		this->cntSubmarine = 0;
+		this->yaht = 0;
+		this->speeBoat = 0;
+		cout << "destructor " << this << "\n";
+	}*/
 };
 
