f57f4f637cbdbf643e0d2c5b441827b84b88e822
Andrey-dev-CJ <andrey.godowanets@gmail.com>
2021-09-23 21:53:59 +0300

    test new class

--- Main.cpp
+++ Main.cpp
@@ -5,16 +5,23 @@
 using namespace std;
 int main() {
 
-    Ship First("Heck"),Second("shake");
+    //Ship First("Heck",15,0,2),Second("shake");
+    //Ship buf;
     int a[4] = { 0,8,6,4 };
     int* mas = a;
     List<int>Name(mas,4);
     cout << Name;
-    List<Ship>MyList(First);
-    MyList.append(Second);
-    List<Ship>MyList2(MyList);
-    for (int i = 0; i < MyList2.getSize(); i++)
+    //list<Ship>MyList(First);
+    //1List<Ship>MySecList;
+    //MySecList = MyList;
+    
+
+    //MyList.append(Second);
+    /*for (int i = 0; i < MyList2.getSize(); i++) {
         MyList2[i].save();
+    }*/
+    Ship Ships;
+    List<Submarine> L;
     system("pause");
     return 0;
 
