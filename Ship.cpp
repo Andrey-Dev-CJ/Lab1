ce44218eb291a72f22e81d795fd590b60d55a69d
Andrey-dev-CJ <andrey.godowanets@gmail.com>
2021-10-08 05:25:54 +0300

    added new methods for users

--- Ship.cpp
+++ Ship.cpp
@@ -1,7 +1,7 @@
 #include "Ship.h"
 #include <fstream>
 #include <filesystem>
-
+using namespace std;
 Ship::Ship()
 {
 	this->init();
@@ -9,77 +9,230 @@ Ship::Ship()
 
 void Ship::init()
 {
-	ifstream f;
+	ifstream f,f2;
 	ofstream of;
 	string str, nstr;
 	nstr = "";
 	int cntYaht,cntSubmarine,cntSpeedboat,pos;
-
-	//автоматический ввод
-	f.open("DATA/PATH.pth");
-	if (f) {
-		if (f.peek() != EOF) {
-			while (getline(f, str))
-			{
-				//парсер
-				cout << str << "\n";
-				if (str.find("Yaht")!= std::string::npos) {
-					cntYaht = cntShips(str);
-
-				}
-				else if (str.find("Submarine")!= std::string::npos) {
-					cntSubmarine = cntShips(str);
-
-				}
-				else {
-					cntSpeedboat = cntShips(str);
-				}
-
-			}	
-			f.close();
-		}
-		else {//ручной ввод
-			f.close();
-			this->handInput(of, "DATA/PATH.pth");
-		}
-	}
-
-	//ручной ввод
-	else  {
-		this->handInput(of, "DATA/PATH.pth");
-	}
 	
-	//после ввода проводим заполнение 
 
-	//заполнение листа €хтами
-	f.open("DATA/Yaht.ob");
-	int i = 0;
-	if (f.peek() != EOF) {
+	//посчитать 
+
+//при запуске программы считывание obj файлов и создание pth файла на основе данных
+//вывод содержимого pth
+
+	if (checkFiles("DATA/Yaht.obj")==1) {
+		//успешное открытие файла
+		//считывание из файла
+		int i = 0;
+		f2.open("DATA/Ships.pth");
+		while (getline(f2, str)) {
+			if(str.find("Speedboat")!=-1)
+				cntSpeedboat = cntShips(str);
+			else if(str.find("Submarine") != -1)
+				cntSubmarine = cntShips(str);
+			else
+				cntYaht = cntShips(str);
+			
+			if (i == 3)
+				break;
+			i++;
+		}
+		f2.close();
+		i = 0;
+		f.open("DATA/Yaht.obj");
 		while (getline(f, str)) {
 			MyYaht.append(Yaht(str));
-			if (i == cntYaht-1)
+			if (i == cntYaht - 1)
 				break;
 			i++;
 		}
-	}
-	
-	f.close();
+		f.close();
 
-	f.open("DATA/Sboat.ob");
-	i = 0;
-	if (f.peek() != EOF) {
+	}
+	else if (checkFiles("DATA/Yaht.obj")==0) {
+		//пустой файл
+		//ручное заполнение
+		string str;
+		while (1) {
+			MyYaht.append(Yaht(1));
+			cout << "¬водить слдеующую €хту? Enter Y/N: ";
+			cin >> str;
+			if (str == "N" || str == "n")
+				break;
+			
+		}
+	}
+	else {
+		//ошибка открыти€
+		//ручное заполнение
+		string str;
+		while (1) {
+			MyYaht.append(Yaht(1));
+			cout << "¬водить слдеующую €хту? Enter Y/N: ";
+			cin >> str;
+			if (str == "N" || str == "n")
+				break;
+
+		}
+
+	}
+
+	if (checkFiles("DATA/Sboat.obj") == 1) {
+		//успешное открытие файла
+		//считывание из файла
+		int i = 0;
+		f2.open("DATA/Ships.pth");
+		while (getline(f2, str)) {
+			if (str.find("Speedboat") != -1)
+				cntSpeedboat = cntShips(str);
+			else if (str.find("Submarine") != -1)
+				cntSubmarine = cntShips(str);
+			else
+				cntYaht = cntShips(str);
+
+			if (i == 3)
+				break;
+			i++;
+		}
+		f2.close();
+		i = 0;
+		f.open("DATA/Sboat.obj");
 		while (getline(f, str)) {
 			MySpeedboat.append(SpeedBoat(str));
-			if (i ==  cntSpeedboat-1)
+			if (i == cntSpeedboat - 1)
 				break;
 			i++;
 		}
+		f.close();
+
+	}
+	else if (checkFiles("DATA/Sboat.obj") == 0) {
+		//пустой файл
+		//ручное заполнение
+		string str;
+		while (1) {
+			MySpeedboat.append(SpeedBoat(1));
+			cout << "¬водить слдеующий катер? Enter Y/N: ";
+			cin >> str;
+			if (str == "N" || str == "n")
+				break;
+
+		}
+	}
+	else {
+		//ошибка открыти€
+		//ручное заполнение
+		string str;
+		while (1) {
+			MySpeedboat.append(SpeedBoat(1));
+			cout << "¬водить слдеующий катер? Enter Y/N: ";
+			cin >> str;
+			if (str == "N" || str == "n")
+				break;
+
+		}
+
+	}
+	
+	if (checkFiles("DATA/Sub.obj") == 1) {
+		//успешное открытие файла
+		//считывание из файла
+		int i = 0;
+	
+		f2.open("DATA/Ships.pth");
+		while (getline(f2, str)) {
+			if (str.find("Speedboat") != -1)
+				cntSpeedboat = cntShips(str);
+			else if (str.find("Submarine") != -1)
+				cntSubmarine = cntShips(str);
+			else
+				cntYaht = cntShips(str);
+
+			if (i == 3)
+				break;
+			i++;
+		}
+		f.open("DATA/Sub.obj");
+		while (getline(f, str)) {
+			MySubmarine.append(Submarine(str));
+			if (i == cntSubmarine - 1)
+				break;
+			i++;
+		}
+		f.close();
+
+
+	}
+	else if (checkFiles("DATA/Sub.obj") == 0) {
+		//пустой файл
+		//ручное заполнение
+		string str;
+		while (1) {
+			MySubmarine.append(Submarine(1));
+			cout << "¬водить слдеующую лодку? Enter Y/N: ";
+			cin >> str;
+			if (str == "N" || str == "n")
+				break;
+
+		}
+	}
+	else {
+		//ошибка открыти€
+		//ручное заполнение
+		string str;
+		while (1) {
+			MySubmarine.append(Submarine(1));
+			cout << "¬водить слдеующую лодку? Enter Y/N: ";
+			cin >> str;
+			if (str == "N" || str == "n")
+				break;
+
+		}
+
 	}
 
-	f.close();
+	save();
+
+
+
 }
 void Ship::save()
 {
+
+	ofstream of;
+	of.open("DATA/Ships.pth");
+	if (!of) {
+		of.close();
+		of.open("DATA/Ships.pth", ios_base::out | ios_base::app);
+		of << "1. Speedboat|" << MySpeedboat.getSize() << "|;\n";
+		of << "2. Submarine|" << MySubmarine.getSize() << "|;\n";
+		of << "3. Yaht|" << MyYaht.getSize() << "|;\n";
+
+	}
+	else
+	{
+		of << "1. Speedboat|" << MySpeedboat.getSize() << "|;\n";
+		of << "2. Submarine|" << MySubmarine.getSize() << "|;\n";
+		of << "3. Yaht|" << MyYaht.getSize() << "|;\n";
+	}
+	of.close();
+
+
+	of.open("DATA/Sboat.obj");
+	of.close();
+	for (int i = 0; i < MySpeedboat.getSize(); i++)
+		MySpeedboat[i].save();
+
+	of.open("DATA/Sub.obj");
+	of.close();
+	for (int i = 0; i < MySubmarine.getSize(); i++)
+		MySubmarine[i].save();
+
+	of.open("DATA/Yaht.obj");
+	of.close();
+	for (int i = 0; i < MyYaht.getSize(); i++)
+		MyYaht[i].save();
 
 }
 void Ship::recovery()
@@ -108,24 +261,6 @@ void Ship::handInput(ofstream& of, string filename)
 	}
 	of.close();
 }
-void Ship::initNext() {
-	initYahts();
-	initSubmarines();
-	initSpeedboat();
-}
-
-void Ship::initYahts()
-{
-
-}
-
-void Ship::initSubmarines()
-{
-}
-
-void Ship::initSpeedboat()
-{
-}
 
 int Ship::cntShips(string str)
 {	
@@ -141,13 +276,150 @@ int Ship::cntShips(string str)
 	return stoi(nstr);
 }
 
-//void Ship::save()
-//{
-//	std::ofstream fout("ship.txt");
-//	fout <<"Submarin:" <<this->cntSubmarine<<std::endl;
-//	fout << "Yaht:" << this->yaht << std::endl;
-//	fout << "SpeedBoat:" << this->speeBoat << std::endl;
-//	fout << "name:" << this->name << std::endl;
-//	fout.close();
-//}
+int Ship::checkFiles(string path)
+{
+	//если всЄ ок, то 1 вернЄтс€
+	//если нету какого-то файла - 0
+	
+	ifstream f;
+
+	f.open(path);
+	if (!f)
+		return -1;
+	else {
+		if (f.peek() == EOF) {
+			return 0;
+		}
+	}
+	f.close();
+	return 1;
+}
+
+void Ship::show()
+{
+	showYahts();
+	showSub();
+	showSboat();
+}
+
+void Ship::showYahts()
+{
+	cout << "\n-----------------Yahts----------------\n";
+	for (int i = 0; i < MyYaht.getSize(); i++) {
+		cout << "[" << i << "]:" << endl;
+		MyYaht[i].show();
+	}
+	cout<<endl;
+}
+
+void Ship::showSboat()
+{
+	cout << "\n-----------------Speed boats----------------\n";
+	for (int i = 0; i < MySpeedboat.getSize(); i++) {
+		cout << "[" << i << "]:" << endl;
+		MySpeedboat[i].show();
+	}
+	cout << endl;
+}
+
+void Ship::showSub()
+{
+	cout << "\n-----------------Submarines----------------\n";
+	for (int i = 0; i < MySubmarine.getSize(); i++) {
+		cout << "[" << i << "]:" << endl;
+		MySubmarine[i].show();
+	}
+
+	cout << endl;
+}
+
+void Ship::addNewYaht()
+{
+	MyYaht.append(Yaht(1));
+}
+
+void Ship::addNewSboat()
+{
+	MySpeedboat.append(SpeedBoat(1));
+}
+
+void Ship::addNewSub()
+{
+	MySubmarine.append(Submarine(1));
+}
+
+void Ship::removeYaht(int i)
+{
+	try {
+		MyYaht.irase(i);
+		ofstream of;
+		of.open("DATA/Yaht.obj");
+		of.close();
+		for (int i = 0; i < MyYaht.getSize(); i++)
+			MyYaht[i].save();
+	}
+	catch (int a) {
+		cout << "Not deleted\n";
+	}
+
+	catch (string s)
+	{
+		cout << s;
+	}
+
+}
+
+void Ship::removeSboat(int i)
+{
+	try {
+		MySpeedboat.irase(i);
+		ofstream of;
+		of.open("DATA/Sboat.obj");
+		of.close();
+		for (int i = 0; i < MySpeedboat.getSize(); i++)
+			MySpeedboat[i].save();
+	}
+	catch (int a) {
+		cout << "Not deleted\n";
+	}
+
+	catch (string s)
+	{
+		cout << s;
+	}
+}
+
+void Ship::removeSub(int i)
+{
+	try {
+		MySubmarine.irase(i);
+		ofstream of;
+		of.open("DATA/Sub.obj");
+		of.close();
+		for (int i = 0; i < MySubmarine.getSize(); i++)
+			MySubmarine[i].save();
+	}
+	catch (int a) {
+		cout << "Not deleted\n";
+	}
+
+	catch (string s)
+	{
+		cout << s;
+	}
+}
+int Ship::getCntYahts()
+{
+	return MyYaht.getSize();
+}
+
+int Ship::getCntSub()
+{
+	return MySubmarine.getSize();
+}
+
+int Ship::getCntSboat()
+{
+	return MySpeedboat.getSize();
+}
 
--- Ship.h
+++ Ship.h
@@ -23,62 +23,23 @@ public:
 	void save();
 	void recovery();
 	void handInput(ofstream& of, string filename);
-	void initNext();
-	void initYahts();
-	void initSubmarines();
-	void initSpeedboat();
-	int cntShips(string str);
-	/*int cntSubmarine;
-	int yaht ;
-	int speeBoat;
-	string name;
-	Ship(const Ship& obj) {
-		cout << "copy constructor " << this << endl;
-		this->cntSubmarine = obj.cntSubmarine;
-		this->speeBoat = obj.speeBoat;
-		this->yaht = obj.yaht;
-		this->name = obj.name;
-	
-	}
-	void save();
-	Ship() {
-		cout << "standart constructor " << this << endl;
-		cntSubmarine = 0;
-		yaht = 0;
-		speeBoat = 0;
-		string name = "no name";
-	}
-	Ship(string val) {
-		cout << "constructor with 1 parm " << this << endl;
-		this->name = val;
-		this->cntSubmarine = 5;
-		this->yaht = 4;
-		this->speeBoat = 3;
+	int cntShips(string str);// parse string Ship|cnt|;
+	int checkFiles(string path);
+	void show();
+	void showYahts();
+	void showSboat();
+	void showSub();
 
-	}
+	void addNewYaht();
+	void addNewSboat();
+	void addNewSub();
 
-	Ship(string val, int cnt, int cnt2, int cnt3) {
-		cout << "constructor with many parm " << this << endl;
-		this->name = val;
-		this->cntSubmarine = cnt;
-		this->yaht = cnt2;
-		this->speeBoat = cnt3;
+	void removeYaht(int i);
+	void removeSboat(int i);
+	void removeSub(int i);
 
-	}
-	Ship& operator=(const Ship& obj) {
-		
-		this->name = obj.name;
-		this->cntSubmarine = obj.cntSubmarine;
-		this->yaht = obj.yaht;
-		this->speeBoat = obj.speeBoat;
-		return *this;
-	}
-	~Ship() {
-		this->name = "";
-		this->cntSubmarine = 0;
-		this->yaht = 0;
-		this->speeBoat = 0;
-		cout << "destructor " << this << "\n";
-	}*/
+	int getCntYahts();
+	int getCntSub();
+	int getCntSboat();
 };
 
